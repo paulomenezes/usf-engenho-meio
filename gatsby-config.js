5b0415b645009c8c221cdf0ad946de11ca7d2918
C1320662 Paulo Henrique Guimaraes de Menezes <c1320662@interno.bb.com.br>
2021-08-22 14:10:46 -0300

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby + Node.js (TypeScript) API',
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Gatsby + Node.js (TypeScript) API',
+        short_name: 'Gatsby + Node.js (TypeScript)',
+        start_url: '/',
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+  ],
+};
